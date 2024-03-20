e616dd270ac9c290b06a0650060b2303ff997808
Antonio P <letual@outlook.es>
2024-03-20 14:39:26 +0100

    navbar-v1

--- containers/navbar/navbar.php
+++ containers/navbar/navbar.php
@@ -2,33 +2,36 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="navbar.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg fixed-top bg-light navbar-light">
-  <div class="container">
-    <a class="navbar-brand" href="#"><img id="medacLogo" src="assets\logo-vector\MEDAC_imagotipo_horiz_azul_RGB.svg" alt="Logotipo de MEDAC" draggable="false" height="30" /></a>
+ 
+    <div class="container"> 
+         <a class="navbar-brand" href="#"><img id="medacLogo" src="assets\logo-vector\MEDAC_imagotipo_horiz_azul_RGB.svg" alt="Logotipo de MEDAC" draggable="false" height="45" /></a>
+
+                <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#n_bar" aria-controls="navbarNavAltMarkup" aria-label="Toggle navigation">
+                    
+                    <i class="fas fa-bars"></i>
+                </button>
 
 
-
-    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <i class="fas fa-bars"></i>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse" id="n_bar">
       <ul class="navbar-nav ms-auto align-items-center">
         <li class="nav-item">
-          <a class="nav-link mx-2" href="#!"><i class="fas fa-plus-circle pe-2"></i>Inicio</a>
+          <a class="nav-link mx-3" href="#!">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link mx-2" href="#!"><i class="fas fa-bell pe-2"></i>Visita virtual</a>
+          <a class="nav-link mx-3" href="#!">Visita virtual</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link mx-2" href="#!"><i class="fas fa-heart pe-2"></i>Galería</a>
+          <a class="nav-link mx-3" href="#!">Galería</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link mx-2" href="#!"><i class="fas fa-heart pe-2"></i>Contacto</a>
+          <a class="nav-link mx-3" href="#!">Contacto</a>
         </li>
         <li class="nav-item ms-3">
-          <a id="inicioSesionBoton" class=" badge rounded-pill" href="#!">Iniciar sesión</a>
+          <a id="inicioSesionBoton" class="btn badge rounded-pill" href="#!">Iniciar sesión</a>
         </li>
       </ul>
     </div>
