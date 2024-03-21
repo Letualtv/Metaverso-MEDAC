29f6d5f82656d1a7feb1c39f5e93305e1ee4c913
Antonio P <letual@outlook.es>
2024-03-21 11:29:29 +0100

    Favicon
    Se añade favicon

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>MEDAC IGUAL</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Noto Sans' rel='stylesheet'>
+    <link rel="icon" href="assets\logo-vector\MEDAC_isotipo_azul_RGB.svg" type="image/x-icon">
 
 </head>
 <body>
