3c0660a6ede44728ef772a89a1dee10dda3ef194
Antonio P <letual@outlook.es>
2024-03-21 14:48:44 +0100

    c

--- containers/navbar/navbar.php
+++ containers/navbar/navbar.php
@@ -8,16 +8,14 @@
 <nav class="navbar navbar-expand-lg fixed-top bg-light navbar-light">
 
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="assets\logo-fundacion\FUNDACION_MEDAC_imagotipo_horizontal_azul_RGB.svg" alt="Logotipo de MEDAC" draggable="false" height="65" /></a>
-
-    <button id="btnHamburger" class="navbar-toggler me-3 py-2" type="button" data-bs-toggle="collapse" data-bs-target="#n_bar" aria-controls="n_bar" aria-expanded="false" aria-label="Toggle navigation">
-
+    <button id="btnHamburger" class="navbar-toggler ml-3" type="button" data-bs-toggle="collapse" data-bs-target="#n_bar" aria-controls="n_bar" aria-expanded="false" aria-label="Toggle navigation">
       <i onclick="changeI(this)" id="iHamburger" class="fa-solid fa-bars  fa-lg"></i>
     </button>
 
-
-    <div class="collapse navbar-collapse" id="n_bar">
-      <ul class="navbar-nav ms-auto align-items-left">
+    <div class="collapse navbar-collapse " id="n_bar">
+      <a class=" mt-2 mt-lg-0" href="#">
+        <img class="mx-auto d-block" src="assets\logo-fundacion\FUNDACION_MEDAC_imagotipo_horizontal_azul_RGB.svg" alt="Logotipo de MEDAC" loading="lazy" height="65" /></a>
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item ">
           <a class="nav-link mx-3" href="#!">Inicio</a>
         </li>
@@ -30,13 +28,13 @@
         <li class="nav-item">
           <a class="nav-link mx-3" href="#!">Contacto</a>
         </li>
-        
+
       </ul>
     </div>
-    <div class="d-flex align-items-center">
-          <a  href="#!"><button id="btnEntrar" class="btn rounded-pill">Entrar
-          </button></a>
-</div> 
+    <div class=" mx-1 float-end">
+      <a class="" href="#!"><button id="btnEntrar" class="btn rounded-pill">Entrar
+        </button></a>
+    </div>
   </div>
 </nav>
 
