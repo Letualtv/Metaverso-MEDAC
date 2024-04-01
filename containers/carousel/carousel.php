8beb7e0e2ab929ea97a5a94488baca34b4d10c79
Antonio P <letual@outlook.es>
2024-04-01 16:33:11 +0200

    cambios carousel y mejora de video

--- containers/carousel/carousel.php
+++ containers/carousel/carousel.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" href="carousel.css">
 
 <!-- Carousel -->
-<section class="container mb-5 mt-3">
+<section id="carousel" class="container mb-5 mt-3">
     
     <div id="carouselExampleDark" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
@@ -27,105 +27,105 @@
         </div>
 
         <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="10000">
+            <div class="carousel-item active" >
                 <img src="assets\assembly\carousel\Backstage1.png" class="d-block w-100" alt="Backstage">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Backstage</h5>
                     <p>Zona de preparación para salir a escenario.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item" >
                 <img src="assets\assembly\carousel\Backstage2.png" class="d-block w-100" alt="Backstage">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Backstage</h5>
                     <p>Algunos útiles para los últimos retoques.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item" >
                 <img src="assets\assembly\carousel\Backstage3.png" class="d-block w-100" alt="Backstage">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Backstage</h5>
                     <p>Puerta de acceso/salida a la sala.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item" >
                 <img src="assets\assembly\carousel\Cafeteria1.png" class="d-block w-100" alt="Cafeteria">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Cafetería</h5>
                     <p>Nuestra cafetería, ideal para charlar o reponer energía.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item" >
                 <img src="assets\assembly\carousel\Cafeteria2.png" class="d-block w-100" alt="Cafeteria">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Cafetería</h5>
                     <p>Vista desde tras la barra.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item" >
                 <img src="assets\assembly\carousel\Control1.png" class="d-block w-100" alt="Sala de control">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Sala de control</h5>
                     <p>Luces, pantallas y equipo técnico listo para el espectáculo.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item">
                 <img src="assets\assembly\carousel\Control2.png" class="d-block w-100" alt="Sala de control">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Salón de actos</h5>
                     <p>Acceso a la sala de control.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item">
                 <img src="assets\assembly\carousel\Edificio1.png" class="d-block w-100" alt="Edificio">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Edificio</h5>
                     <p>Nuestro fantástico edificio.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item">
                 <img src="assets\assembly\carousel\Edificio2.png" class="d-block w-100" alt="Edificio">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Interior</h5>
                     <p>Interior de la parte de recepción de nuestro edificio.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item" >
                 <img src="assets\assembly\carousel\Edificio3.png" class="d-block w-100" alt="Edificio">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Edificio</h5>
                     <p>Vista aérea de nuestro edificio.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item" >
                 <img src="assets\assembly\carousel\Platea1.png" class="d-block w-100" alt="Acceso a platea">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Platea</h5>
                     <p>Entrada a platea.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item" >
                 <img src="assets\assembly\carousel\Platea2.png" class="d-block w-100" alt="Acceso a platea">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Platea</h5>
                     <p>Entrada a platea vista desde abajo.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item" >
                 <img src="assets\assembly\carousel\Recepcion2.png" class="d-block w-100" alt="Recepcion">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Accesos</h5>
                     <p>Pasillo por el que acceder a las escaleras o al ascensor.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item" >
                 <img src="assets\assembly\carousel\Recepcion3.png" class="d-block w-100" alt="Recepcion">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Recepción</h5>
                     <p>Nuestra amplia recepción.</p>
                 </div>
             </div>
-            <div class="carousel-item" data-bs-interval="10000">
+            <div class="carousel-item" >
                 <img src="assets\assembly\carousel\Salon2.png" class="d-block w-100" alt="Salon de actos">
                 <div class="carousel-caption d-none d-md-block">
                     <h5 class="fw-bold">Salón de actos</h5>
@@ -136,11 +136,11 @@
 
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
+            <span class="visually-hidden">Anterior</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
+            <span class="visually-hidden">Siguiente</span>
         </button>
 
     </div>
