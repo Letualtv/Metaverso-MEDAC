80cf056c3209c95ea7cb4af07fab2bc97df6a562
Antonio P. <letual@outlook.es>
2024-04-18 19:00:28 +0200

    BIG UPDATE

--- index.php
+++ index.php
@@ -17,34 +17,39 @@
     <link rel="stylesheet" href="containers/gallery/gallery.css">
     <link rel="stylesheet" href="containers/video/video.css">
     <link rel="stylesheet" href="containers/contact/contact.css">
-    <link rel="stylesheet" href="containers/aside/aside.css">
+    <link rel="stylesheet" href="containers/faq/faq.css">
     <link rel="stylesheet" href="containers/footer/footer.css">
     <link rel="stylesheet" href="containers/cards/cards.css">
     <link rel="stylesheet" href="app.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-<body>
+<body id="body">
 
     <?php require('./containers/navbar/navbar.html') ?>
 
     <section id="mainContainer" class="container-fluid">
         <div class="row">
             <div class="col-md-6 col-xs-12 m-auto mainContent align-middle">
-                <h1 class="text-center pt-3 ">¡Sumérgete en el Metaverso!</h1>
+                <h1 class="h1 text-center pt-3 ">¡Sumérgete en el Metaverso!</h1>
                 <p class="text-center p-3 ">Bienvenidos al fascinante mundo del <b class="blue">Metaverso MEDAC</b> desarrollado por nuestros alumnos. <br/>
                     Os invitamos a explorar las diversas secciones y salas que componen este innovador <b class="blue">campus virtual</b> en el que podrás disfrutar de la compañía de tus compañeros, asistir a clases, ponencias, etc.
                 <p class="text-center fw-bold blue">¡Todo sin moverte de casa!</p>
                 </p>
-                <button id="titleButton" class="btn mx-auto d-flex my-5">ENTRA AL METAVERSO</button>
+                <button id="titleButton" class="btn rounded-pill mx-auto d-flex my-5">ENTRA AL METAVERSO</button>
             </div>
             <div class="col-md-5 col-xs-12 ">
-                <img src="assets/assembly/web/imagen1rb.png" alt="Imagen de ejemplo" class="img-fluid imgMeta d-flex mx-auto">
+                <img src="assets/assembly/web/imagen1rb.png" alt="Imagen de metaverso Medac" class="img-fluid imgMeta d-flex mx-auto">
             </div>
         </div>
     </section>
 
 
-
+  <!-- Boton desplazamiento -->
+  <div id="div-totop" class="cmn-divfloat">
+    <a href="#body" class="btn  cmn-btncircle">
+      <i class="fa fa-arrow-up align-middle text-white"></i>
+    </a>
+  </div>
 
 
     <!-- ENLACE COMPONENTES -->
