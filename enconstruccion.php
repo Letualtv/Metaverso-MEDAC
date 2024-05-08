bf8eeefecb0cb04551e85f0cd69b0420c73f5ef7
Antonio P. <letual@outlook.es>
2024-05-08 12:13:26 +0200

    Cambios de navbar

---
+++ enconstruccion.php
@@ -0,0 +1,34 @@
+<link
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+  rel="stylesheet"
+  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+  crossorigin="anonymous"
+/>
+
+<section class="container-fluid bg-dark" id="enconstruccion">
+  <div
+    class="position-absolute top-50 start-50 translate-middle bg-warning p-5 "
+    style="width: 80vh"
+  >
+    <h2 class="fw-bold pb-3">
+      La página solicitada está actualmente en construcción
+    </h2>
+    <a href="index.php">
+      <button class="btn btn-rounded btn-danger">
+        Volver a la página principal
+      </button>
+    </a>
+  </div>
+</section>
+
+<style>
+  #enconstruccion {
+    min-height: 100vh;
+  }
+</style>
+
+<script
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+  crossorigin="anonymous"
+></script>
