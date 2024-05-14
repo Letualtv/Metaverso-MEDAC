e9401c2e82e8ec406015f51fae234c6974e9d2ef
Antonio P. <letual@outlook.es>
2024-05-14 10:37:56 +0200

    Formulario que funciona

--- containers/contact/contact.js
+++ containers/contact/contact.js
@@ -1,54 +1,48 @@
-const form = document.getElementById('contactForm');
-const alert = document.querySelector('.alert');
+const form = document.getElementById("contactForm");
+const alert = document.querySelector(".alert");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPSNGFYh6A4zz27B3vUu4H2pqBXtGLq2Q",
-  authDomain: "formulariomedac.firebaseapp.com",
-  databaseURL: "https://formulariomedac-default-rtdb.firebaseio.com",
-  projectId: "formulariomedac",
-  storageBucket: "formulariomedac.appspot.com",
-  messagingSenderId: "538716580446",
-  appId: "1:538716580446:web:39dfaee90e617c39d6a2cf",
-  measurementId: "G-85R9FCV943"
+  apiKey: "AIzaSyA2-kpMCTcymfPJbEWWv8cJKpUh1vAkGvg",
+  authDomain: "medacverso-a8852.firebaseapp.com",
+  databaseURL: "https://medacverso-a8852-default-rtdb.firebaseio.com/",
+  projectId: "medacverso-a8852",
+  storageBucket: "medacverso-a8852.appspot.com",
+  messagingSenderId: "565098211725",
+  appId: "1:565098211725:web:581ca0b96eae21be1375c4",
+  measurementId: "G-58FE7BH6GJ",
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+const database = firebase.database();
 
-const database = firebase.database()
+const ref = database.ref("messages");
 
-const ref = database.ref('messages')
-
-form.addEventListener('submit',(e)=>{
-
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const nombre = document.getElementById('nombre').value;
-  const telefono = document.getElementById('telefono').value;
-  const email = document.getElementById('email').value;
-  const cp = document.getElementById('cp').value;
-  const pregunta = document.getElementById('pregunta').value;
-  const check1 = document.getElementById('check1').checked;
-  const check2 = document.getElementById('check2').checked;
+  const nombre = document.getElementById("nombre").value;
+  const telefono = document.getElementById("telefono").value;
+  const email = document.getElementById("email").value;
+  const cp = document.getElementById("cp").value;
+  const pregunta = document.getElementById("pregunta").value;
+  const check1 = document.getElementById("check1").checked;
+  const check2 = document.getElementById("check2").checked;
 
+  ref.push({
+    nombre: nombre,
+    telefono: telefono,
+    email: email,
+    cp: cp,
+    pregunta: pregunta,
+    check1: check1,
+    check2: check2,
+  });
 
-ref.push({
-  nombre:nombre,
-  telefono:telefono,
-  email:email,
-  cp:cp,
-  pregunta:pregunta,
-  check1:check1,
-  check2:check2,
-})
-
-  alert.style.display='block'
-  setTimeout(()=>{
-    alert.style.display="none"
-  },5000)
-  form.reset()
-})
-
-
-
+  alert.style.display = "block";
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 5000);
+  form.reset();
+});
